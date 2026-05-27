ac00a8d04dda90dcec5c1fe0c4b9a2a6581ce434
NBlancs <bnoel.omar@gmail.com>
2026-05-28 01:06:38 +0800

    added cors for backend onrender

--- config/settings.py
+++ config/settings.py
@@ -152,6 +152,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "https://ipt-pitfrontend.onrender.com",
     "https://ipt-pit.vercel.app",
+    "https://ipt-pitbackend.onrender.com",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:8000"
